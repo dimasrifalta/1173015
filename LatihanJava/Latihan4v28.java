2106bc106ae48ac1fa3b2e0359bd5e1f7a4e6fd9
dimasrifalta <dimasrifalta@gmail.com>
2019-10-24 08:34:11 +0700

    Latihan 10-24-2019

---
+++ LatihanJava/Latihan4v28.java
@@ -0,0 +1,27 @@
+public class Latihan4v28 {
+
+    public static void main(String[] args) {
+
+        // while (kondisi) {
+        // aksi
+        // }
+
+        int a = 0;
+        boolean kondisi = true;
+
+        System.out.println("awal program");
+
+        while (kondisi) {
+            System.out.println("while loop ke-" + a);
+
+            if (a == 10) {
+                kondisi = false;
+            }
+
+            a++;
+        }
+
+        System.out.println("akhir program");
+
+    }
+}
